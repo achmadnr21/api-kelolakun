716ca8363f1d06a3b69f62ab17b7dad2db2920c5
achmadnr21 <achmad.riskynanda01@gmail.com>
2023-06-19 08:31:26 +0700

    update : CategoryController

--- app/Http/Controllers/api/CategoryController.php
+++ app/Http/Controllers/api/CategoryController.php
@@ -28,4 +28,80 @@ class CategoryController extends Controller
             ]
         ]);
     }
+
+    public function store(Request $request){
+        $dataCategory = new Category;
+        $rules = [
+            'name' => 'required',
+        ];
+        $validator = Validator:: make($request->all(),$rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'fail',
+                'data' => $validator->errors()
+            ]);
+        }
+
+
+        // 'category_id',
+        // 'name'
+
+        $dataCategory->category_id = $request->category_id;
+        $dataCategory->name = $request->name;
+
+        $post = $dataCategory->save();
+        return response()->json([
+            'status' => 'success',
+            'data' => 'insert success!'
+        ]);
+
+    }
+
+    public function update(Request $request, string $id){
+        $dataCategory = Category::find($id);
+        if(empty($dataCategory)){
+            return response()->json([
+                'status' => 'success',
+                'data' => 'data not found'
+            ], 404);
+        }
+        $rules = [
+
+        ];
+        $validator = Validator:: make($request->all(),$rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'fail',
+                'data' => $validator->errors()
+            ]);
+        }
+        
+
+        $dataCategory->category_id = $request->category_id;
+        $dataCategory->name = $request->name;
+
+        $post = $dataCategory->save();
+        return response()->json([
+            'status' => 'success',
+            'data' => 'update success!'
+        ]);
+    }
+
+
+    public function destroy(string $id)
+    {
+         $dataCategory = Category::find($id);
+        if(empty($dataCategory)){
+            return response()->json([
+                'status' => 'success',
+                'data' => 'data not found'
+            ], 404);
+        }
+
+        $post = $dataCategory->delete();
+        return response()->json([
+            'status' => 'success',
+            'data' => 'deletion success!'
+        ]);   
+    }
 }
