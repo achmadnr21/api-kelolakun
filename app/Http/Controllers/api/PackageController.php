6d8cf5cd2bda9bcb7e55cb9e2ddc3fe1bbca343b
achmadnr21 <achmad.riskynanda01@gmail.com>
2023-06-19 08:36:39 +0700

    update : PackageController

--- app/Http/Controllers/api/PackageController.php
+++ app/Http/Controllers/api/PackageController.php
@@ -29,4 +29,83 @@ class PackageController extends Controller
             ]
         ]);
     }
+
+    public function store(Request $request){
+        $dataPackage = new Package;
+        $rules = [
+            'total_weight' => 'required',
+            'total_price' => 'required'
+        ];
+        $validator = Validator:: make($request->all(),$rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'fail',
+                'data' => $validator->errors()
+            ]);
+        }
+        
+        // 'package_id',
+        // 'total_weight',
+        // 'total_price'
+
+        $dataPackage->package_id = $request->package_id;
+        $dataPackage->total_weight = $request->total_weight;
+        $dataPackage->total_price = $request->total_price;
+
+        $post = $dataPackage->save();
+        return response()->json([
+            'status' => 'success',
+            'data' => 'insert success!'
+        ]);
+
+    }
+
+    public function update(Request $request, string $id){
+        $dataPackage = Package::find($id);
+        if(empty($dataPackage)){
+            return response()->json([
+                'status' => 'success',
+                'data' => 'data not found'
+            ], 404);
+        }
+        $rules = [
+
+        ];
+        $validator = Validator:: make($request->all(),$rules);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'fail',
+                'data' => $validator->errors()
+            ]);
+        }
+        
+
+        $dataPackage->package_id = $request->package_id;
+        $dataPackage->total_weight = $request->total_weight;
+        $dataPackage->total_price = $request->total_price;
+
+        $post = $dataPackage->save();
+        return response()->json([
+            'status' => 'success',
+            'data' => 'update success!'
+        ]);
+    }
+
+
+    public function destroy(string $id)
+    {
+         $dataPackage = Package::find($id);
+        if(empty($dataPackage)){
+            return response()->json([
+                'status' => 'success',
+                'data' => 'data not found'
+            ], 404);
+        }
+
+        $post = $dataPackage->delete();
+        return response()->json([
+            'status' => 'success',
+            'data' => 'deletion success!'
+        ]);   
+    }
 }
